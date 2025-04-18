6b8da83184d6b58c27aec5b1e3a54915edb59350
Jacob Ray <jbenjamin.ray@gmail.com>
2025-04-18 01:34:51 -0600

    Changed a couple things. Moved what I wrote into the main.rs for this new Dioxus project.

--- learm_cp/src/main.rs
+++ learm_cp/src/main.rs
@@ -4,7 +4,63 @@ const FAVICON: Asset = asset!("/assets/favicon.ico");
 const MAIN_CSS: Asset = asset!("/assets/main.css");
 const HEADER_SVG: Asset = asset!("/assets/header.svg");
 
+#[derive(Debug)]
+pub struct Servo {
+    pub pos_max: u16,
+    pub pos_min: u16,
+    pub current_pos: u16,
+    pub description: String,
+}
+
+#[derive(Debug)]
+pub struct LeArm {
+    pub servo_1: Servo,
+    pub servo_2: Servo,
+    pub servo_3: Servo,
+    pub servo_4: Servo,
+    pub servo_5: Servo,
+    pub servo_6: Servo,
+}
 fn main() {
+    let mut le_arm = LeArm {
+        servo_1: Servo {
+            pos_max: 2500,
+            pos_min: 1500,
+            current_pos: 2000, // TODO: Figure out what the servo home positions are
+            description: String::from("Gripper, Joint 6"),
+        },
+        servo_2: Servo {
+            pos_max: 2500,
+            pos_min: 500,
+            current_pos: 2000, // TODO: Figure out what the servo home positions are
+            description: String::from("Joint 5"),
+        },
+        servo_3: Servo {
+            pos_max: 2500,
+            pos_min: 500,
+            current_pos: 2000, // TODO: Figure out what the servo home positions are
+            description: String::from("Joint 4"),
+        },
+        servo_4: Servo {
+            pos_max: 2500,
+            pos_min: 500,
+            current_pos: 2000, // TODO: Figure out what the servo home positions are
+            description: String::from("Joint 3"),
+        },
+        servo_5: Servo {
+            pos_max: 2500,
+            pos_min: 500,
+            current_pos: 2000, // TODO: Figure out what the servo home positions are
+            description: String::from("Joint 2"),
+        },
+        servo_6: Servo {
+            pos_max: 2500,
+            pos_min: 500,
+            current_pos: 2000, // TODO: Figure out what the servo home positions are
+            description: String::from("Base, Joint 1"),
+        },
+    };
+
     dioxus::launch(App);
 }
 
